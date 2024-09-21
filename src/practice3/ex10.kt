ecf7692b0a9f31df5790bf7ced37285803b5fb5b
Laptop <laptop@xyz.com>
2024-09-21 19:33:28 +0900

    ex9-10

--- src/practice3/ex10.kt
+++ src/practice3/ex10.kt
@@ -1,5 +1,11 @@
 package practice3
 
 fun main() {
-    
+    val a = readln().toInt()
+    val b = readln().toInt()
+
+    println(when {
+        a % b == 0 -> "Число b является делителем"
+        else -> "Число b не является делителем"
+    })
 }
--- src/practice3/ex9.kt
+++ src/practice3/ex9.kt
@@ -3,7 +3,5 @@ package practice3
 fun main() {
     val a = readln()
 
-    for (i in a.length-1 downTo 0) {
-        print(a[i])
-    }
+    println(a.reversed())
 }
